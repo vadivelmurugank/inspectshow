5390ad079d8cdf047830b3d6af91be0f919bb5c5
vadivelmurugank <vadivelmurugank@gmail.com>
2015-08-24 21:51:46 -0700

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
   name = 'inspectshow',
-  packages = ['inspectshow'],
   version = '0.1',
   description = 'show module internals with inspect in tree format',
   author = 'Vadivel',
   author_email = 'vadivelmurugank@gmail.com',
-  url = 'https://github.com/vadivelmurugank/inspectshow'
+  url = 'https://github.com/vadivelmurugank/inspectshow',
+  license = 'MIT',
 )
