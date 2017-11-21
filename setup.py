3441ca8ab4090f2f1f32ccdfdcdb9f873b82462e
vadivelmurugank <vadivelmurugank@gmail.com>
2017-11-21 14:25:21 -0800

    README.rst

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 
-from distutils.core import setup
+#from distutils.core import setup
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
 setup(
   name = 'inspectshow',
@@ -11,4 +17,29 @@ setup(
   author_email = 'vadivelmurugank@gmail.com',
   url = 'https://github.com/vadivelmurugank/inspectshow',
   license = 'MIT',
+
+   # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
