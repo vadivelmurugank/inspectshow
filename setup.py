b7b2c4038dd0260d492e283cbeea24ce1c5fbae6
vadivelmurugank <vadivelmurugank@gmail.com>
2017-11-21 15:17:38 -0800

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,43 @@
+VERSION = '1.1'
+DESCRIPTION = 'show module internals and corresponding class internals in tree format'
 
-#from distutils.core import setup
+# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Python Software Foundation License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Software Development',
+]
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+if __name__ == '__main__':
+    try:
+        from setuptools import setup, find_packages
+        from codecs import open
+        from os import path
+    except ImportError:
+        from distutils.core import setup
+
+    with open('README.rst', 'r') as f:
+        readme = f.read()
 
 setup(
   name = 'inspectshow',
   packages_dir = {'inspectshow': ' '},
   packages = ['inspectshow'],
-  version = '1.0',
-  description = 'show module internals and corresponding class internals in tree format',
+  version = VERSION,
+  description = DESCRIPTION,
+  long_description=readme,
   author = 'Vadivel',
   author_email = 'vadivelmurugank@gmail.com',
   url = 'https://github.com/vadivelmurugank/inspectshow',
   license = 'MIT',
-
-   # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+  classifiers=classifiers,
+  keywords='inspect show tree',
 )
